060eb97e6db8665c4a444b0ef437ed35aa9b4ad8
hwna00 <layton3534@gmail.com>
2022-11-18 22:31:18 +0900

    JWT Token

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     path("", views.Users.as_view()),
     path("me/", views.Me.as_view()),
     path("@<str:username>", views.PublicUser.as_view()),
-    path("log-in", views.Login.as_view()),
+    path("log-in", views.Login.as_view()),  # cookie
     path("log-out", views.Logout.as_view()),
-    path("token-login", obtain_auth_token),
+    path("token-login", obtain_auth_token),  # auth token
+    path("jwt-login", views.JWTLogin.as_view()),
     path("change-password", views.ChangePassword.as_view()),
 ]
