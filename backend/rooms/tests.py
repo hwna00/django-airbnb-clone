c907d62593b381cc788c359b343fbc9e59816dff
hwna00 <layton3534@gmail.com>
2022-11-19 00:29:24 +0900

    Create Ameity Test

--- backend/rooms/tests.py
+++ backend/rooms/tests.py
@@ -1,6 +1,71 @@
 from rest_framework.test import APITestCase
+from . import models
 
 
 class TestAmenities(APITestCase):
-    def test_two_plus_two(self):
-        self.assertEqual(2 + 2, 4, "The math is wrong")
+
+    NAME = "Amentiy Test"
+    DESC = "Amenity Des"
+
+    URL = "/api/v1/rooms/amenities/"
+
+    def setUp(self):
+        models.Amenity.objects.create(
+            name=self.NAME,
+            description=self.DESC,
+        )
+
+    def test_all_amenities(self):
+        response = self.client.get(self.URL)
+        data = response.json()
+        self.assertEqual(
+            response.status_code,
+            200,
+            "Status code isn't 200.",
+        )
+        self.assertIsInstance(
+            data,
+            list,
+        )
+        self.assertEqual(
+            len(data),
+            1,
+        )
+        self.assertEqual(
+            data[0]["name"],
+            self.NAME,
+        )
+        self.assertEqual(
+            data[0]["description"],
+            self.DESC,
+        )
+
+    def test_create_amenity(self):
+
+        new_amenity_name = "New Amenity"
+        new_amenity_description = "New Amenity desc"
+
+        response = self.client.post(
+            self.URL,
+            data={
+                "name": new_amenity_name,
+                "description": new_amenity_description,
+            },
+        )
+        data = response.json()
+        self.assertEqual(
+            response.status_code,
+            200,
+            "Not 200 status Code",
+        )
+        self.assertEqual(
+            data["name"],
+            new_amenity_name,
+        )
+        self.assertEqual(
+            data["description"],
+            new_amenity_description,
+        )
+        response = self.client.post(self.URL)
+        self.assertEqual(response.status_code, 400)
+        print(response.json())
