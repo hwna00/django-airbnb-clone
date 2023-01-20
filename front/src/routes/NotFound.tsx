0b66dedeff4f1da415d25a3c4da49511e69b2804
hwna00 <layton3534@gmail.com>
2023-01-20 22:15:19 +0900

    Frontend Setup

---
+++ front/src/routes/NotFound.tsx
@@ -0,0 +1,15 @@
+import { Button, Heading, Link, Text, VStack } from '@chakra-ui/react';
+
+export default function NotFound() {
+  return (
+    <VStack bg="gray.100" justifyContent={'center'} minH="100vh">
+      <Heading>Page Not Found</Heading>
+      <Text>It seems that you're lost.</Text>
+      <Link href="/">
+        <Button variant={'link'} colorScheme={'red'}>
+          GO HOME &rarr;
+        </Button>
+      </Link>
+    </VStack>
+  );
+}
