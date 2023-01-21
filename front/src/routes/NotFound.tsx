6cbbc57edb6f98846929c52b422b8494bc73ead2
hwna00 <layton3534@gmail.com>
2023-01-21 16:27:28 +0900

    Header + Login Modal

--- front/src/routes/NotFound.tsx
+++ front/src/routes/NotFound.tsx
@@ -1,15 +1,15 @@
-import { Button, Heading, Link, Text, VStack } from '@chakra-ui/react';
+import { Button, Heading, Link, Text, VStack } from '@chakra-ui/react'
 
 export default function NotFound() {
   return (
     <VStack bg="gray.100" justifyContent={'center'} minH="100vh">
       <Heading>Page Not Found</Heading>
-      <Text>It seems that you're lost.</Text>
+      <Text>It seems that youre lost.</Text>
       <Link href="/">
         <Button variant={'link'} colorScheme={'red'}>
           GO HOME &rarr;
         </Button>
       </Link>
     </VStack>
-  );
+  )
 }
